76692e3f52ce2e7e71bf2db5c46f8191a4a2339a
gabrielly reis <gabyreis88@gamil.com>
2024-09-04 22:21:56 -0300

    terceiro dia

---
+++ src/dados.js
@@ -0,0 +1,19 @@
+let dados = ["item1", "item2", "item3"];
+
+let item1 = {
+    titulo : 'item1',
+    descricao : 'descricao1', 
+    link : 'link1'
+}
+
+let item2 = {
+    titulo : 'item2',
+    descricao : 'descricao2', 
+    link : 'link2'
+}
+
+let item3 = {
+    titulo : 'item3',
+    descricao : 'descricao3', 
+    link : 'link3'
+}
